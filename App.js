89db8b3eebd8d4d79f213f3a172b55b1490addc8
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2020-08-07 15:31:17 -0300

    Criando os componentes da aplicação

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {SafeAreaView,StyleSheet,Text,} from 'react-native';
+import {SafeAreaView,StyleSheet,Text} from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 const App: () => React$Node = () => {
   return (
@@ -12,6 +13,19 @@ const App: () => React$Node = () => {
         Tamanho da grade:
         {params.getRowsAmout()}x{params.getColumnsAmout()}
       </Text>
+
+      <Field/>
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
+      <Field mined />
+      <Field mined opened/>
+      <Field mined opened exploded/>
+      <Field flagged/>
+      <Field flagged opened/>
+
     </SafeAreaView>
   );
 };
@@ -20,6 +34,7 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     justifyContent: 'center',
+    // justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
